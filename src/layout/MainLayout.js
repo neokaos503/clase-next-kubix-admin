37bf919e757f33782ad17effa57ac88b8c502cdd
Willian Morales <you@example.comwilliamazin@gmail.com>
2022-11-07 16:26:37 -0600

    se agrego header y main

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,19 +1,18 @@
-import Header from '@components/Header';
-import Nav from '@common/Nav';
-
-
+import Header from "@components/header";
 
 export default function MainLayout({children}) {
     return(
 <>
-<div className="min-h-full">7
-<Header/>
-<Nav/>
+<div className="min-h-full">
+ 
 <main>
+   <Header/>
     <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
 {children}
     </div>
+   
 </main>
+
 </div>
 </>
     );
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,14 @@
-import {ProviderAuth} from '@hooks/useAuth';
 import MainLayout from '@layout/MainLayout';
 import '@styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
   return (
 <>
-<ProviderAuth>
+
 <MainLayout>
 <Component {...pageProps}/>
   </MainLayout>
-</ProviderAuth>
+
   
   </>
   );
@@ -17,3 +16,6 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
+
+
